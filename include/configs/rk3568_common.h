e08919199fd39cb3818bd5980fe039e827dcfc73
Nazarii Tupitsa <two_nazya@mail.ru>
2024-06-21 20:01:12 +0300

    Update rk3568_common.h

--- include/configs/rk3568_common.h
+++ include/configs/rk3568_common.h
@@ -87,8 +87,8 @@
 #define ENV_BOOTMENU_ENTRIES \
 	"bootcmd=setenv stdin serial,pwr_key_stdin,touch_keys; bootmenu 15\0" \
 	"bootmenu_0=Search for extlinux.conf on all partitions=run scan_dev_for_boot_part\0" \
-	"bootmenu_1=Boot OS1=sysboot mmc 0:8 any ${scriptaddr} /boot/extlinux/extlinux.conf\0" \
-	"bootmenu_2=Boot OS2=sysboot mmc 0:9 any ${scriptaddr} /boot/extlinux/extlinux.conf\0"
+	"bootmenu_1=Boot Android=boot_android mmc 0\0" \
+	"bootmenu_2=Reboot=reboot\0" \
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	ENV_MEM_LAYOUT_SETTINGS \
